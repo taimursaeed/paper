4424cf54dc273856725ff1645d46c70a4d8bdba7
taimursaeed <taimur.saeed.malik@gmail.com>
2021-12-27 10:05:53 +0500

    handled no result case in search articles

--- src/features/search/searchedArticles.js
+++ src/features/search/searchedArticles.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button, Flex, Icon, Select, Spinner } from "@chakra-ui/react";
+import { Button, Flex, Icon, Select, Spinner, Text } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { reverseArticles } from "../home/homeSlice";
 import { fetchSearchedArticles, selectError, selectSearchedArticles, selectStatus } from "./searchSlice";
@@ -38,42 +38,44 @@ export default function SearchedArticles() {
       </Flex>
     );
   } else if (status === "succeeded") {
-    content = <ArticleSection articles={searchedArticles}/>;
+    content = searchedArticles.results.length > 0 ? <ArticleSection articles={searchedArticles}/> :
+      <Text align="center">No article found</Text>;
   } else if (status === "error") {
     content = { error };
   }
 
   return (
     <>
-      <Flex justifyContent="flex-end" mb="-12">
-        <Button colorScheme="blue" pl="4" pr="4" mr="4">
-          <Icon width="1rem" mr="1">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-              ></path>
-            </svg>
-          </Icon>
-          Bookmarks
-        </Button>
-        <Select
-          size="md"
-          onChange={handleOrder}
-          defaultValue="newest"
-          width="200px"
-        >
-          <option value="newest">Newest First</option>
-          <option value="oldest">Oldest First</option>
-        </Select>
-      </Flex>
+      {searchedArticles.results.length > 0 ?
+        <Flex justifyContent="flex-end" mb="-12">
+          <Button colorScheme="blue" pl="4" pr="4" mr="4">
+            <Icon width="1rem" mr="1">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                ></path>
+              </svg>
+            </Icon>
+            Bookmarks
+          </Button>
+          <Select
+            size="md"
+            onChange={handleOrder}
+            defaultValue="newest"
+            width="200px"
+          >
+            <option value="newest">Newest First</option>
+            <option value="oldest">Oldest First</option>
+          </Select>
+        </Flex> : ""}
       {content}
     </>
   );
