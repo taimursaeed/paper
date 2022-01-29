ec7c8e4d456059e9bdceb6043a64ea130146aca0
taimursaeed <taimur.saeed.malik@gmail.com>
2022-01-29 19:17:17 +0500

    cleanup footer.js

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import { Text, Flex } from "@chakra-ui/react";
-
-export default function Footer() {
-  return (
-    <Flex justifyContent="center" bg="blue.500" py="4" color="white">
-      <Text> &copy; Paper 2021</Text>
-    </Flex>
-  );
-}
